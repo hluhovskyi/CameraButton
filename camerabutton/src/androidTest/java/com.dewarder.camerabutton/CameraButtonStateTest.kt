d156874b559e6bcaba56f3f9ead0e9cf87bc79eb
Artem Hluhovskyi <dewarder@gmail.com>
2017-12-10 00:16:08 +0200

    Add rest of button state tests

--- camerabutton/src/androidTest/java/com.dewarder.camerabutton/CameraButtonStateTest.kt
+++ camerabutton/src/androidTest/java/com.dewarder.camerabutton/CameraButtonStateTest.kt
@@ -22,10 +22,13 @@ import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.dewarder.camerabutton.CameraButton.State
 import com.dewarder.camerabutton.util.disposeHoldActions
 import com.dewarder.camerabutton.util.pressAndHold
+import com.dewarder.camerabutton.util.release
 import com.dewarder.camerabutton.util.state
 import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -37,6 +40,15 @@ class CameraButtonStateTest {
     val activityRule: ActivityTestRule<CameraButtonActivity>
         = ActivityTestRule(CameraButtonActivity::class.java)
 
+    @Before
+    fun setUp() {
+        activityRule.activity.button.apply {
+            expandDelay = EXPAND_DELAY
+            expandDuration = EXPAND_DURATION
+            collapseDuration = COLLAPSE_DURATION
+        }
+    }
+
     @After
     fun tearDown() {
         disposeHoldActions()
@@ -46,23 +58,62 @@ class CameraButtonStateTest {
     fun testButtonOnPressHasPressedState() {
         onView(withId(android.R.id.button1))
             .perform(pressAndHold())
-            .check(matches(state(CameraButton.State.PRESSED)))
+            .check(matches(state(State.PRESSED)))
     }
 
     @Test
     fun testButtonOnHoldAfterDelayHasStartExpandingState() {
-        val testExpandDelay = 200L
-        val testExpandDuration = 400L
-
-        activityRule.activity.button.apply {
-            expandDelay = testExpandDelay
-            expandDuration = testExpandDuration
-        }
-
         onView(withId(android.R.id.button1))
             .perform(pressAndHold())
-        sleep(testExpandDelay)
+        sleep(EXPAND_DELAY)
         onView(withId(android.R.id.button1))
-            .check(matches(state(CameraButton.State.START_EXPANDING)))
+            .check(matches(state(State.START_EXPANDING)))
+    }
+
+    @Test
+    fun testButtonOnHoldAfterDelayHasExpandedState() {
+        onView(withId(android.R.id.button1))
+            .perform(pressAndHold())
+        sleep(EXPAND_DELAY + EXPAND_DURATION)
+        onView(withId(android.R.id.button1))
+            .check(matches(state(State.EXPANDED)))
+    }
+
+    @Test
+    fun testButtonReleaseWithoutDelayHasDefaultState() {
+        onView(withId(android.R.id.button1))
+            .perform(pressAndHold())
+            .perform(release())
+            .check(matches(state(State.DEFAULT)))
+    }
+
+    @Test
+    fun testButtonReleaseAfterDelayHasStartCollapsingState() {
+        onView(withId(android.R.id.button1))
+            .perform(pressAndHold())
+        sleep(EXPAND_DELAY + EXPAND_DURATION)
+        onView(withId(android.R.id.button1))
+            .check(matches(state(State.EXPANDED)))
+            .perform(release())
+            .check(matches(state(State.START_COLLAPSING)))
+    }
+
+    @Test
+    fun testButtonReleaseAfterDelayHasDefaultState() {
+        onView(withId(android.R.id.button1))
+            .perform(pressAndHold())
+        sleep(EXPAND_DELAY + EXPAND_DURATION)
+        onView(withId(android.R.id.button1))
+            .perform(release())
+        sleep(COLLAPSE_DURATION)
+        onView(withId(android.R.id.button1))
+            .check(matches(state(State.DEFAULT)))
+
+    }
+
+    private companion object {
+        private const val EXPAND_DELAY = 200L
+        private const val EXPAND_DURATION = 400L
+        private const val COLLAPSE_DURATION = 400L
     }
 }
